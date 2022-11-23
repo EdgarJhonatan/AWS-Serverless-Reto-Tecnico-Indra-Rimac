0241840d128e14d6402b6b0a39f7d0dad1afcf55
Edgar Delgadillo <jdmkrd@gmail.com>
2022-11-23 13:38:56 -0500

    cambios actualizacion

--- src/handler.js
+++ src/handler.js
@@ -10,8 +10,8 @@ const tableVehicle = process.env.TABLE_VEHICLE || "vehicle";
 
 // Ordernar los items de cada tabla.
 function sortBtId(a, b) {
-  if (a.createdAt > b.createdAt) return -1;
-  return 1;
+   if (a.createdAt > b.createdAt) return -1;
+   return 1;
 }
 
 /**
@@ -22,81 +22,83 @@ function sortBtId(a, b) {
 
 /** REALIZA LA QUERY A DYNAMODB, SI EXISTE LO MUESTRA, SINO  CONSULTA (API -STAR WARS) Y GUARDA */
 module.exports.createStarship = async (event, context, callback) => {
-  try {
-    const numberStarship = event.pathParameters.number;
-    const res = await dynamo.getByID(md5(numberStarship), tableStarship);
-    console.log("Respuesta del dynamoDb", res);
-    if (!res || !res.Item) {
-      const starShip = await consumer.getStarships(numberStarship);
-      console.log("Respuesta The Star Wars API startShips", starShip);
-      const { statusCode, body } = starShip;
-      if (statusCode != 200) {
-        throw new Error("Error al consumir servicio The Star Wars API");
+   try {
+      const numberStarship = event.pathParameters.number;
+      const res = await dynamo.getByID(md5(numberStarship), tableStarship);
+      console.log("Respuesta del dynamoDb", res);
+      if (!res || !res.Item) {
+         const starShip = await consumer.getStarships(numberStarship);
+         console.log("Respuesta The Star Wars API startShips", starShip);
+         const { statusCode, body } = starShip;
+         if (statusCode != 200) {
+            throw new Error("Error al consumir servicio The Star Wars API");
+         } else {
+            const dataInput = {
+               id: md5(numberStarship),
+               createAt: new Date().toISOString(),
+               ...JSON.parse(body),
+            };
+            await dynamo.saveItem(tableStarship, dataInput);
+            //return callback(null, responses._201(dataInput));
+            return responses._201(dataInput);
+         }
       } else {
-        const dataInput = {
-          id: md5(numberStarship),
-          createAt: new Date().toISOString(),
-          ...JSON.parse(body),
-        };
-        await dynamo.saveItem(tableStarship, dataInput);
-        return callback(null, responses._201(dataInput));
+         //return callback(null, responses._200(res.Item));
+         return responses._200(res.Item);
       }
-    } else {
-      return callback(null, responses._200(res.Item));
-    }
-  } catch (error) {
-    console.log("Ingresó al catch", error);
-    return callback(null, responses._500({ message: error.message }));
-  }
+   } catch (error) {
+      console.log("Ingresó al catch", error);
+      return callback(null, responses._500({ message: error.message }));
+   }
 };
 
 /** REALIZA LA QUERY A DYNAMODB, OBTIENE TODOS LOS DATOS GUARDADOS  */
 module.exports.getAllStarship = async (event, context, callback) => {
-  try {
-    const res = await dynamo.getAll(tableStarship);
-    return callback(null, responses._200(res.Items.sort(sortBtId)));
-  } catch (error) {
-    return callback(null, responses._500({ message: error.message }));
-  }
+   try {
+      const res = await dynamo.getAll(tableStarship);
+      return callback(null, responses._200(res.Items.sort(sortBtId)));
+   } catch (error) {
+      return callback(null, responses._500({ message: error.message }));
+   }
 };
 
 /** REALIZA LA QUERY A DYNAMODB DE CIERTO RANGO DE REGISTROS */
 module.exports.getRangeLimitStarship = async (event, context, callback) => {
-  try {
-    const numberStartship = event.pathParameters.number;
-    const res = await dynamo.getItemLimit(tableStarship, numberStartship);
-    return callback(null, responses._200(res.Items.sort(sortBtId)));
-  } catch (error) {
-    return callback(null, responses._500(error));
-  }
+   try {
+      const numberStartship = event.pathParameters.number;
+      const res = await dynamo.getItemLimit(tableStarship, numberStartship);
+      return callback(null, responses._200(res.Items.sort(sortBtId)));
+   } catch (error) {
+      return callback(null, responses._500(error));
+   }
 };
 
 /** REALIZA LA QUERY A DYNAMODB, ACTUALIZA SI EXISTE, SINO NO ACTUALIZA */
 module.exports.putStarship = async (event, context, callback) => {
-  try {
-    const idStarship = event.pathParameters.id;
-    const dataForm = JSON.parse(event.body);
-    const paramName = dataForm.paramName;
-    const paramValue = dataForm.paramValue;
+   try {
+      const idStarship = event.pathParameters.id;
+      const dataForm = JSON.parse(event.body);
+      const paramName = dataForm.paramName;
+      const paramValue = dataForm.paramValue;
 
-    console.log("dataForm", dataForm, idStarship);
+      console.log("dataForm", dataForm, idStarship);
 
-    await dynamo.updateItem(idStarship, tableStarship, paramName, paramValue);
-    return callback(null, responses._200({ message: "Update Success" }));
-  } catch (error) {
-    return callback(null, responses._500(error));
-  }
+      await dynamo.updateItem(idStarship, tableStarship, paramName, paramValue);
+      return callback(null, responses._200({ message: "Update Success" }));
+   } catch (error) {
+      return callback(null, responses._500(error));
+   }
 };
 
 /** REALIZA LA QUERY A DYNAMODB, ELIMINA EL REGISTRO */
 module.exports.deleteStarship = async (event, context, callback) => {
-  try {
-    const idStarship = event.pathParameters.id;
-    await dynamo.deleteIten(idStarship, tableStarship);
-    return callback(null, responses._200({ message: "Delete Success" }));
-  } catch (error) {
-    return callback(null, responses._500(error));
-  }
+   try {
+      const idStarship = event.pathParameters.id;
+      await dynamo.deleteIten(idStarship, tableStarship);
+      return callback(null, responses._200({ message: "Delete Success" }));
+   } catch (error) {
+      return callback(null, responses._500(error));
+   }
 };
 
 /**
@@ -107,77 +109,77 @@ module.exports.deleteStarship = async (event, context, callback) => {
 
 /** REALIZA LA QUERY A DYNAMODB, SI EXISTE LO MUESTRA, SINO  CONSULTA (API -STAR WARS) Y GUARDA */
 module.exports.createVehicle = async (event, context, callback) => {
-  try {
-    const numberVehicle = event.pathParameters.number;
-    const res = await dynamo.getByID(md5(numberVehicle), tableVehicle);
-    console.log("Respuesta del dynamoDb", res);
-    if (!res || !res.Item) {
-      const starVehicle = await consumer.getVehicle(numberVehicle);
-      console.log("Respuesta The Star Wars API starVehicle", starVehicle);
-      const { statusCode, body } = starVehicle;
-      if (statusCode != 200) {
-        throw new Error("Error al consumir servicio The Star Wars API");
+   try {
+      const numberVehicle = event.pathParameters.number;
+      const res = await dynamo.getByID(md5(numberVehicle), tableVehicle);
+      console.log("Respuesta del dynamoDb", res);
+      if (!res || !res.Item) {
+         const starVehicle = await consumer.getVehicle(numberVehicle);
+         console.log("Respuesta The Star Wars API starVehicle", starVehicle);
+         const { statusCode, body } = starVehicle;
+         if (statusCode != 200) {
+            throw new Error("Error al consumir servicio The Star Wars API");
+         } else {
+            const dataInput = {
+               id: md5(numberVehicle),
+               createAt: new Date().toISOString(),
+               ...JSON.parse(body),
+            };
+            await dynamo.saveItem(tableVehicle, dataInput);
+            return callback(null, responses._201(dataInput));
+         }
       } else {
-        const dataInput = {
-          id: md5(numberVehicle),
-          createAt: new Date().toISOString(),
-          ...JSON.parse(body),
-        };
-        await dynamo.saveItem(tableVehicle, dataInput);
-        return callback(null, responses._201(dataInput));
+         return callback(null, responses._200(res.Item));
       }
-    } else {
-      return callback(null, responses._200(res.Item));
-    }
-  } catch (error) {
-    console.log("Ingresó al catch", error);
-    return callback(null, responses._500({ message: error.message }));
-  }
+   } catch (error) {
+      console.log("Ingresó al catch", error);
+      return callback(null, responses._500({ message: error.message }));
+   }
 };
 
 /** REALIZA LA QUERY A DYNAMODB, OBTIENE TODOS LOS DATOS GUARDADOS  */
 module.exports.getAllVehicle = async (event, context, callback) => {
-  try {
-    const res = await dynamo.getAll(tableVehicle);
-    return callback(null, responses._200(res.Items.sort(sortBtId)));
-  } catch (error) {
-    return callback(null, responses._500({ message: error.message }));
-  }
+   try {
+      const res = await dynamo.getAll(tableVehicle);
+      return callback(null, responses._200(res.Items.sort(sortBtId)));
+   } catch (error) {
+      return callback(null, responses._500({ message: error.message }));
+   }
 };
 
 /** REALIZA LA QUERY A DYNAMODB DE CIERTO RANGO DE REGISTROS */
 module.exports.getRangeLimitVehicle = async (event, context, callback) => {
-  try {
-    const numberVehicle = event.pathParameters.number;
-    const res = await dynamo.getItemLimit(tableVehicle, numberVehicle);
-    return callback(null, responses._200(res.Items.sort(sortBtId)));
-  } catch (error) {
-    return callback(null, responses._500(error));
-  }
+   try {
+      const numberVehicle = event.pathParameters.number;
+      const res = await dynamo.getItemLimit(tableVehicle, numberVehicle);
+      return callback(null, responses._200(res.Items.sort(sortBtId)));
+   } catch (error) {
+      return callback(null, responses._500(error));
+   }
 };
 
 /** REALIZA LA QUERY A DYNAMODB, ACTUALIZA SI EXISTE, SINO NO ACTUALIZA */
 module.exports.putVehicle = async (event, context, callback) => {
-  try {
-    const idVehicle = event.pathParameters.id;
-    const dataForm = JSON.parse(event.body);
-    const paramName = dataForm.paramName;
-    const paramValue = dataForm.paramValue;
+   try {
+      const idVehicle = event.pathParameters.id;
+      const dataForm = JSON.parse(event.body);
+      const paramName = dataForm.paramName;
+      const paramValue = dataForm.paramValue;
 
-    await dynamo.updateItem(idVehicle, tableVehicle, paramName, paramValue);
-    return callback(null, responses._200({ message: "Update Success" }));
-  } catch (error) {
-    return callback(null, responses._500(error));
-  }
+      await dynamo.updateItem(idVehicle, tableVehicle, paramName, paramValue);
+      return callback(null, responses._200({ message: "Update Success" }));
+   } catch (error) {
+      return callback(null, responses._500(error));
+   }
 };
 
 /** REALIZA LA QUERY A DYNAMODB, ELIMINA EL REGISTRO */
 module.exports.deleteVehicle = async (event, context, callback) => {
-  try {
-    const idVehicle = event.pathParameters.id;
-    await dynamo.deleteIten(idVehicle, tableVehicle);
-    return callback(null, responses._200({ message: "Delete Success" }));
-  } catch (error) {
-    return callback(null, responses._500(error));
-  }
+   try {
+      const idVehicle = event.pathParameters.id;
+      await dynamo.deleteIten(idVehicle, tableVehicle);
+      return callback(null, responses._200({ message: "Delete Success" }));
+   } catch (error) {
+      return callback(null, responses._500(error));
+   }
 };
